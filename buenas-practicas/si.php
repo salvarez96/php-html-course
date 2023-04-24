9b8164a4b6f99314ad95f7953b951bd9a4862a34
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-04-23 19:06:34 -0500

    Clase 9: refactorizando con las mejores prácticas

--- buenas-practicas/si.php
+++ buenas-practicas/si.php
@@ -1,3 +1,14 @@
+<?php
+
+$tablaDel9 = [];
+for ($i = 1; $i <= 10; $i++) {
+  $result = 9 * $i;
+  $text = "9 x $i = $result";
+  array_push($tablaDel9, $text);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,18 +16,18 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>No hagas esto</title>
+  <title>Haz esto</title>
 </head>
 
 <body>
 
   <h1>Tabla del 9</h1>
   <ul>
-    <?php for ($i = 1; $i <= 10; $i++): ?>
+    <?php foreach ($tablaDel9 as $result): ?>
       <li>
-        <?= "9 x $i = " . (9 * $i) ?>
+        <?= $result ?>
       </li>
-    <?php endfor; ?>
+    <?php endforeach; ?>
   </ul>
 
 </body>
